117bb594a36bbe55a85dbaa558b3186ac8007104
dunnomyname <kasimozkul@hotmail.com>
2022-05-07 22:00:10 +0300

    Initial commit.

---
+++ src/errors/database-connection-error.ts
@@ -0,0 +1,16 @@
+import { CustomError } from "./custom-error";
+
+export class DatabaseConnectionError extends CustomError {
+  reason = 'Error connecting to the database!';
+  statusCode = 500;
+
+  constructor() {
+    super('Error connecting to the database!');
+
+    Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+
+  serializeErrors() {
+    return [{ message: this.reason }];
+  }
+}
